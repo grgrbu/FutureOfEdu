55d7ce0cde5acee85f9746099dcf1074fe74b6a7
Vladimir Yakshimamedov <39744184+Arinoir@users.noreply.github.com>
2019-10-26 00:54:40 +0300

    Mage can now use spells.
    Now I need DataBase to learn how to craft spells.

--- app/src/main/java/com/example/mage_battle/player/Mage.java
+++ app/src/main/java/com/example/mage_battle/player/Mage.java
@@ -2,12 +2,36 @@ package com.example.mage_battle.player;
 
 import com.example.mage_battle.inventory.Rune;
 import com.example.mage_battle.inventory.Spell;
+import java.util.ArrayList;
 
 public class Mage {
-    public Rune runeInventory[];
-    public Spell spellInventory[];
+    public ArrayList<Rune> runeInventory;
+    public ArrayList<Spell> spellInventory;
     public int runeMastery[];
     public int hp, barrier;
 
+    public Mage(int health, int runeMageMastery[]){
+        this.spellInventory = new ArrayList<>();
+        this.runeInventory = new ArrayList<>();
+        this.hp = health;
+        this.barrier = 0;
+        this.runeMastery = runeMageMastery;
+    }
 
+    public void UseSpell(Mage target, int spellIndex) {
+        if (target.barrier == 1 && this.spellInventory.get(spellIndex).damage > 0) {
+            target.barrier = 0;
+        } else {
+            target.hp -= this.spellInventory.get(spellIndex).damage;
+        }
+        target.hp += this.spellInventory.get(spellIndex).heal;
+        if (this.spellInventory.get(spellIndex).barrier == 1) {
+            target.barrier = 1;
+        }
+        this.spellInventory.remove(spellIndex); //delete spell
+    }
+
+    public void CraftSpell(){
+
+    }
 }
