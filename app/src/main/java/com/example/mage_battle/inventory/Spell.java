d4a3f0bb8a5abaa3d765c97c1e0bd2030375befe
Vladimir Yakshimamedov <39744184+Arinoir@users.noreply.github.com>
2019-10-26 02:08:04 +0300

    I am an idiot

--- app/src/main/java/com/example/mage_battle/inventory/Spell.java
+++ app/src/main/java/com/example/mage_battle/inventory/Spell.java
@@ -1,10 +1,10 @@
 package com.example.mage_battle.inventory;
 
 public class Spell {
-    public String spellName[];
+    public String spellName;
     public int damage, heal, barrier;
 
-    public Spell(String[] name, int dmg, int healing, int barr){
+    public Spell(String name, int dmg, int healing, int barr){
         this.damage = dmg;
         this.heal = healing;
         this.barrier = barr;
