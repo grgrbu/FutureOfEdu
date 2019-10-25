f04f4c57496fb91139a5c6816dd8966bfec8be03
Vladimir Yakshimamedov <39744184+Arinoir@users.noreply.github.com>
2019-10-26 01:57:18 +0300

    Constructor for Rune

--- app/src/main/java/com/example/mage_battle/inventory/Rune.java
+++ app/src/main/java/com/example/mage_battle/inventory/Rune.java
@@ -1,8 +1,15 @@
 package com.example.mage_battle.inventory;
 
+import android.media.Image;
+
 public class Rune {
     public enum RuneType {
         Fire, Earth, Water, Air
     }
     public RuneType runeType;
+    public Image runeImage;
+
+    public Rune(RuneType type){
+        this.runeType = type;
+    }
 }
