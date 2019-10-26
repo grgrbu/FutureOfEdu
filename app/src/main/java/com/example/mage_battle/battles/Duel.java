90c1abdb5772a49b19ab756353bf86d3700ace40
Vladimir Yakshimamedov <39744184+Arinoir@users.noreply.github.com>
2019-10-26 08:52:39 +0300

    New classes and constructors for Duel and PracticeBattle. I now need db.

---
+++ app/src/main/java/com/example/mage_battle/battles/Duel.java
@@ -0,0 +1,13 @@
+package com.example.mage_battle.battles;
+
+import com.example.mage_battle.player.Mage;
+
+public class Duel {
+    public Mage player1, player2;
+    public String namePlayer1, namePlayer2;
+
+    public Duel(String nameplayer1, String nameplayer2){
+        namePlayer1 = nameplayer1;
+        namePlayer2 = nameplayer2;
+    }
+}
---
+++ app/src/main/java/com/example/mage_battle/battles/PracticeBattle.java
@@ -0,0 +1,12 @@
+package com.example.mage_battle.battles;
+
+import com.example.mage_battle.player.Mage;
+
+public class PracticeBattle {
+    public Mage player;
+    public String playerName;
+    public PracticeBattle(String player_name){
+        playerName = player_name;
+
+    }
+}
